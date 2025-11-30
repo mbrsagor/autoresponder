7d299ebd22aab77cf0843d2fc61fe74b375a5ed4
mbrsagor <mbrsagor@gmail.com>
2025-11-30 12:35:48 +0600

    Enhance settings configuration: Integrate environment variables for sensitive settings, switch database to PostgreSQL, and add static/media file handling. Configure Django Message Framework and authentication settings. Implement SMTP configuration for email backend.

--- autoresponder/settings.py
+++ autoresponder/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
+import environ
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-usc0f(2p#o7)*t+89$(%8ak=e(v3unb2ns!9j1#1x%hvmsi1@z"
+env = environ.Env(DEBUG=(bool, False), ALLOWED_HOSTS=list)
+env_path = os.path.join(BASE_DIR, ".env")
+environ.Env.read_env(env_path)
+
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = env("DEBUG")
+ALLOWED_HOSTS = env("ALLOWED_HOSTS")
 
 
 # Application definition
@@ -54,7 +60,7 @@ ROOT_URLCONF = "autoresponder.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -75,8 +81,12 @@ WSGI_APPLICATION = "autoresponder.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USERNAME"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
     }
 }
 
@@ -115,9 +125,34 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# Django Message Framework
+MESSAGE_TAGS = {
+    messages.INFO: "SUCCESS",
+    messages.ERROR: "ERROR",
+    messages.WARNING: "WARNING",
+}
+
+# Auth configuration
+AUTH_USER_MODEL = "user.User"
+LOGIN_URL = "/user/signin/"
+LOGIN_REDIRECT_URL = "user/signin/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# SMTP CONFIG
+EMAIL_BACKEND = "utils.backend.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = env("EMAIL_USE_TLS")
